57ad7ed944ee04b40fdd22cbff144d2dccf3a94c
Rahul Maurya <rahulmaurya2648@gmail.com>
2026-05-02 04:04:37 +0530

    fix render port

--- app.py
+++ app.py
@@ -215,3 +215,8 @@ def predict():
 if __name__ == '__main__':
     print("\n🏠  House Price Predictor  —  http://127.0.0.1:5000\n")
     app.run(debug=True, port=5000)
+    import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
